2460852d95ea3a19335f843dbb8ed29860478246
buildsample <buildsampletest@gmail.com>
2015-05-01 10:58:20 +0530

    Update sample_test.go

--- src/github.com/OneRedOak/sample/sample_test.go
+++ src/github.com/OneRedOak/sample/sample_test.go
@@ -10,7 +10,7 @@ var _ = Describe("Sample", func() {
     Describe("Adding and retreving Person object from MongoDB", func() {
         Context("inspecting their name", func() {
             It("should result 'Ale'", func() {
-                Expect(GetResult()).To(Equal("Ale"))
+                Expect(GetResult()).To(Equal("Alen"))
             })
         })
     })
